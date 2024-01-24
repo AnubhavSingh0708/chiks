fb4a5bab39ed66c9937316a5ffa73e7a83beb01e
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2024-01-24 12:24:45 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('chicks-store').then((cache) => cache.addAll([
+        '/',
+        '/offline.html',
+        '/favicon.ico',
+        '/favicon.png',
+        '/favicon1.png',
+        '/favicon.svg',
+      ])),
+    );
+  });
+  /*
+  self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+      caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+  });*/
+  self.addEventListener("fetch", (event) => {
+    // Only call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+      event.respondWith(
+        (async () => {
+          try {
+            // First, try to use the navigation preload response if it's
+            // supported.
+            const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
+              return preloadResponse;
+            }
+  
+            // Always try the network first.
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
+          } catch (error) {
+            // catch is only triggered if an exception is thrown, which is
+            // likely due to a network error.
+            // If fetch() returns a valid HTTP response with a response code in
+            // the 4xx or 5xx range, the catch() will NOT be called.
+            console.log("Fetch failed; returning offline page instead.", error);
+  
+            const cache = await caches.open("chicks-store");
+            const cachedResponse = await cache.match("/offline.html");
+            return cachedResponse;
+          }
+        })()
+      );
+    }
+  
+  });
+  
